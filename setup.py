e8ec9bbc5aea7263d3be1ba28c81a6b1032f880a
Xavier Grangier <x.grangier@liberation.fr>
2014-12-27 01:01:32 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""Setup script for django-mustache"""
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='django-mustache',
+    version='0.1',
+    packages=find_packages(exclude=['test', 'tests',
+                                    'example', 'demo']),
+    include_package_data=True,
+    license='BSD License',
+    description='Mustache templates server side rendering in Django',
+    long_description=open('README').read(),
+    author='Djaz Team',
+    author_email='devweb@liberation.fr',
+    url='http://www.liberation.fr/',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ])
